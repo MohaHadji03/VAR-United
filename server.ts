8990b24e258a4114476dd026fb67483e73318dcf
Herrera Santibanez Nicolas (s117195) <nicolas.herrerasantibanez@student.ap.be>
2025-04-23 14:19:00 +0000

    begin van serverside

---
+++ server.ts
@@ -0,0 +1,25 @@
+import express from 'express';
+import path from 'path';
+
+// Create express app
+const app = express();
+
+// Set up EJS
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Basic test route
+app.get('/', (req, res) => {
+    res.render('landingspagina'); // Make sure views/index.ejs exists
+});
+
+// Start server
+app.set('port', 3000);
+app.listen(app.get('port'), () => {
+    console.log('[server] http://localhost:' + app.get('port'));
+});
