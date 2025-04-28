5db30c716dafa7c85f28837a0d738997ef2f514e
Herrera Santibanez Nicolas (s117195) <nicolas.herrerasantibanez@student.ap.be>
2025-04-28 09:10:58 +0000

    sessions en pageroutes aangemaakt

---
+++ session.ts
@@ -0,0 +1,36 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+import session from "express-session";
+import connectMongo from "connect-mongodb-session";
+import { ObjectId } from "mongodb";
+import { FlashMessage, UserModel } from "./interface";
+const MongoDBStore = connectMongo(session);
+
+const mongoStore = new MongoDBStore({
+    uri: process.env.MONGODB_URI ?? "mongodb://localhost:27017/VAR-United-db",
+    collection: "sessions",
+    databaseName: "VAR-United-db",
+});
+
+mongoStore.on("error", (error) => {
+    console.error(error);
+});
+
+export const sessionMiddleware = session({
+    secret: process.env.SESSION_SECRET ?? "your_secret_key",
+    resave: true,
+    saveUninitialized: true,
+    store: mongoStore,
+    cookie: {
+        maxAge: 1000 * 60 * 60 * 24,
+    },
+});
+
+declare module 'express-session' {
+    interface SessionData {
+        loggedIn: boolean;
+        message?: FlashMessage;
+        user?: UserModel;
+    }
+}
