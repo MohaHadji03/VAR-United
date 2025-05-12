4afc63c5c579277a6066371acdfd6285a5a5d3b5
Herrera Santibanez Nicolas (s117195) <nicolas.herrerasantibanez@student.ap.be>
2025-05-12 08:04:31 +0000

    interfaces voor de api

--- interface.ts
+++ interface.ts
@@ -11,3 +11,32 @@ export interface FlashMessage {
     type: "error" | "success"
     message: string;
 }
+
+export interface Club {
+  id: number;
+  name: string;
+  league: number;
+}
+
+export interface League {
+  id: number;
+  name: string;
+  nationId: number;
+}
+
+export interface Nations {
+    id: number;
+    name: string;
+}
+
+export interface Player {
+  id: number;
+  name: string;
+  firstName: string;
+  lastName: string;
+  position: string;
+  rating: number;
+  club: number;
+  league: number;
+  nation: number;
+}
