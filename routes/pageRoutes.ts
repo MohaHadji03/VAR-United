f69d96ffe583f847b570509e588683a79b1b92da
ebrar gencturk <s120520@ap.be>
2025-05-19 07:55:13 +0000

    routes homepage ok

--- routes/pageRoutes.ts
+++ routes/pageRoutes.ts
@@ -35,5 +35,55 @@ export function pageRoutes() {
             res.status(500).send('Internal Server Error');
         }
     });
+
+    router.get('/aboutus', (req, res) => {
+        res.render('aboutus', { currentPage: 'aboutus', title: 'Over ons', user: req.session.user });
+    });
+
+    router.get('/clubs', (req, res) => {
+        res.render('clubs', { currentPage: 'clubs', title: 'Clubs', user: req.session.user });
+    });
+
+    router.get('/competities', (req, res) => {
+        res.render('competities', { currentPage: 'competities', title: 'Competities', user: req.session.user });
+    });
+
+    router.get('/teams', (req, res) => {
+        res.render('teams', { currentPage: 'teams', title: 'Teams', user: req.session.user });
+    });
+
+    router.get('/blacklist', (req, res) => {
+        res.render('tussenpaginablacklist', { currentPage: 'tussenpaginablacklist', title: 'Blacklist tussenpagina', user: req.session.user });
+    });
+    
+router.get('/blacklist-clubs', (req, res) => {
+        res.render('blacklist-clubs', { currentPage: 'blacklist-clubs', title: 'Blacklist Clubs', user: req.session.user });
+    });
+
+
+    router.get('/blacklist-competities', (req, res) => {
+        res.render('blacklist-competities', { currentPage: 'blacklist-competities', title: 'Blacklist Competities', user: req.session.user });
+    });
+
+    router.get('/favoriet', (req, res) => {
+        res.render('tussenpaginafavoriet', { currentPage: 'tussenpaginafavoriet', title: 'Favoriet tussenpagina', user: req.session.user });
+    });
+
+    router.get('/favoriet-clubs', (req, res) => {
+        res.render('favoriet-clubs', { currentPage: 'favoriet-clubs', title: 'Favoriete Clubs', user: req.session.user });
+    });
+
+    router.get('/favoriet-competities', (req, res) => {
+        res.render('favoriet-competities', { currentPage: 'favoriet-competities', title: 'Favoriete Competities', user: req.session.user });
+    });
+
+    router.get('/quizpagina', (req, res) => {
+        res.render('quizpagina', { currentPage: 'quizpagina', title: 'Quiz', user: req.session.user });
+    });
+
+    router.get('/quiz-player', (req, res) => {
+        res.render('player-quiz', { currentPage: 'player-quiz', title: 'Speler Quiz', user: req.session.user });
+    });
+
     return router;
 }
